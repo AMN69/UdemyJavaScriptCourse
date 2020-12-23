d2862b283d2cfa00745e3903b7f6fa8944d7dbd8
Andreu Martínez <martinez.andreu@gmail.com>
2020-12-23 21:38:01 +0100

    Code challenge 1 completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -42,15 +42,61 @@ if (population > 33000000) {
     console.log(population + " population is " + (33000000 - population) + " million below average.");
 };
 
-console.log ('9' - '5');
-console.log ('19' - '13' + '17');
-console.log ('19' - '13' - 17);
+console.log ('9' - '5'); // 4
+console.log ('19' - '13' + '17'); // 617
+console.log ('19' - '13' - 17); // -11
 
 if ('123' < 57) {
     console.log("123 < 57");
 } else {
-    console.log("123 > 57");
+    console.log("123 > 57"); // This
 };
 
-console.log(5 + 6 + '4' + 9 - 4 - 2);
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+
+/*Coding Challenge #1
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK */
+
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
+
+let markBMI1 = markMass / (markHeight ** 2);
+let markBMI2 = markMass / (markHeight * markHeight);
+let johnBMI1 = johnMass / (johnHeight ** 2);
+let johnBMI2 = johnMass / (johnHeight * johnHeight);
+
+let isMarkBMIHigher = markBMI1 > johnBMI1;
+
+// if (markBMI1 > johnBMI1) {
+//     isMarkBMIHigher = true;
+// } else {
+//     isMarkBMIHigher = false;
+// }
+
+console.log("Mark BMI is: ", markBMI1, markBMI2);
+console.log("John BMI is: ", johnBMI1, johnBMI2);
+console.log("Is Mark BMI higher than John's: ", isMarkBMIHigher);
 
