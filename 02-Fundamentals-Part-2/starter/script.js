6d1a77495ddb64399ad682105aa331122222deb0
Andreu Martínez <martinez.andreu@gmail.com>
2020-12-26 20:14:02 +0100

    Javascript course part 2 - coding challenge 2 completed

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -131,4 +131,22 @@ Hint: Remember that an array needs a value in each position, and that value can 
 So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
 GOOD LUCK */
 
+function calcTip (bill) {
+    if (bill < 50 || bill > 300) {
+        return bill * 0.2;
+    } else {
+        return bill * 0.15;
+    };
+}
 
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(tips);
+
+const total = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]];
+
+console.log(total);
