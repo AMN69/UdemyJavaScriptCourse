78a22f2219cfbd4fa46ce5c090e5c067788c87a6
Andreu Martínez <martinez.andreu@gmail.com>
2020-12-26 17:31:05 +0100

    new exercises before coding challenge 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -93,3 +93,42 @@ const checkWinner = (scoreKoalas, scoreDolphins) => {
 console.log(checkWinner(scoreKoalas, scoreDolphins));
 console.log(checkWinner(scoreKoalas2, scoreDolphins2));
 
+const populations = ['Catalonia', 'Spain', 'Finland', 'France'];
+const hasFourElements = populations.length === 4 ? true : false;
+console.log(hasFourElements);
+
+const percentages = [ parseFloat(percentageOfWorld1(7500000)), parseFloat(percentageOfWorld1(47000000)), parseFloat(percentageOfWorld1(6000000)), parseFloat(percentageOfWorld1(58000000))];
+
+console.log(percentages);
+
+let neighbours = ['Germany', 'Spain', 'France', 'Andorra'];
+neighbours.push('Utopia');
+console.log(neighbours);
+
+neighbours.splice(3,1);
+console.log(neighbours);
+
+if (!neighbours.includes('Germany')) {
+    console.log('Probably not a central European country :D');
+};
+
+const countryIndex = neighbours.indexOf('Spain');
+neighbours[countryIndex] = 'King of Spain';
+console.log(neighbours);
+
+/* Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, 
+and if the value is different, the tip is 20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on 
+    the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. 
+    Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data below
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip 
+Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! 
+So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+GOOD LUCK */
+
+
