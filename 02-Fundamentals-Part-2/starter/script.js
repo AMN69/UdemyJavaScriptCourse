e806cadf9a150244b03e422f514a0178c99130c0
Andreu Martínez <martinez.andreu@gmail.com>
2020-12-26 16:33:38 +0100

    Part 2 - coding challenge 1 complete

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -56,7 +56,8 @@ Your tasks:
 1. Create an arrow function'calcAverage'to calculate the average of 3 scores
 2. Use the function to calculate the average for both teams
 3. Create a function'checkWinner'that takes the averages core of each team
-as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)"
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, 
+according to the rule above. Example: "Koalas win (30 vs. 13)"
 4. Use the'checkWinner'function to determine the winner for both Data1 and Data 2
 5. Ignore draws this time
 Test data:
@@ -65,4 +66,30 @@ Hints:
 § To calculate average of 3 values, add them all together and divide by 3
 § To check if number A is at least double number B, check for A >= 2 * B.
 Apply this to the team's average scores 😉 GOOD LUCK */
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+console.log(`First data Dolphins average was ${calcAverage(44, 23, 71)} and Koalas was ${65, 54, 49}`);
+console.log(`Second data Dolphins average was ${calcAverage(85, 54, 41)} and Koalas was ${23, 34, 27}`);
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const scoreDolphins2 = calcAverage(85, 54, 41);
+const scoreKoalas2 = calcAverage(23, 34, 27);
+
+console.log(scoreKoalas2);
+console.log(scoreDolphins2);
+
+const checkWinner = (scoreKoalas, scoreDolphins) => {
+    if (scoreKoalas >= scoreDolphins * 2) {
+        return `Koalas win (${scoreKoalas} vs. ${scoreDolphins})`
+    } else if (scoreDolphins >= scoreKoalas * 2) {
+        return `Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`
+    } else {
+        return 'Nobody wins'
+    }
+}
+
+console.log(checkWinner(scoreKoalas, scoreDolphins));
+console.log(checkWinner(scoreKoalas2, scoreDolphins2));
 
