319a0db8958e9fdffb33d0ba2968d6adbbb83b43
Andreu Martínez <martinez.andreu@gmail.com>
2020-12-27 19:49:39 +0100

    Javascript course - Part1 and 2 completed including coding challenges

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -251,8 +251,97 @@ if (markMiller.BMI > johnSmith.BMI) {
     } else {
         console.log(`${markMiller.fullName}'s BMI (${markMiller.BMI}) is equal than ${johnSmith.fullName}'s (${johnSmith.BMI})!`);
     }
+};
+
+for (let i=1; i<51; i++) {
+    console.log(`Voter number ${i} is currently voting`);
+};
+
+// Copy of populations
+const populations2 = [7500000, 47000000, 6000000, 58000000];
+
+// Copy of percentageOfWorld1
+function percentageOfWorld1Bis(population) {
+    return ((population/7900000000) * 100).toFixed(2);
 }
 
+let percentages2 = [];
+
+for (let i=0; i<populations2.length; i++) {
+    percentages2[i] = parseFloat(percentageOfWorld1Bis(populations2[i]));
+};
+
+console.log(percentages, percentages2);
+
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+
+for (let i=0; i<listOfNeighbours.length; i++) {
+    for (let j=0; j<listOfNeighbours[i].length; j++) {
+        console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+    };
+};
+
+let i=0;
+let percentages3 = [];
+
+while (i<populations2.length) {
+    percentages3[i] = parseFloat(percentageOfWorld1Bis(populations2[i]));
+    i++;
+};
+
+console.log(percentages, percentages3);
+
+/* Coding Challenge #4
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. 
+Use a for loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays 😉 
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the 
+given array. 
+This is a difficult challenge (we haven't done this before)! Here is how to solve it:
+4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. 
+Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. 
+This way, by the end of the loop, you have all values added together
+4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+4.3. Call the function with the 'totals' array
+GOOD LUCK 😀 */
+
+const bills2 = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips2 = [];
+let totals = [];
+
+// function calcTip (bill) {
+//     if (bill < 50 || bill > 300) {
+//         return bill * 0.2;
+//     } else {
+//         return bill * 0.15;
+//     };
+// }
+
+for (let i=0; i<bills2.length; i++) {
+    tips2.push(calcTip(bills2[i]));
+    totals.push(bills2[i]+tips2[i]);
+};
+
+console.log("Bills: ", bills2);
+console.log("Tips: ", tips2);
+console.log("Totals: ", totals);
+
+function calcAverage2(arr) {
+    let totalArr = 0;
+    for (let i=0; i<arr.length; i++) {
+        totalArr = totalArr + arr[i];
+    }
+    return totalArr/arr.length;
+};
+
+console.log("The average is: ", calcAverage2(bills2));
 
 
 
