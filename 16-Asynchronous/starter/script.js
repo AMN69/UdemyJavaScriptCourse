0a3ad2e0842f2418b0fee2804463c8e29e3ed33c
AMN69 <martinez.andreu@gmail.com>
2021-05-25 17:51:53 +0200

    20210525 working on promises with feth and then

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -54,7 +54,10 @@ const renderCountry = function(data, className = '') {
                 </article>
         `;
         countriesContainer.insertAdjacentHTML('beforeend', html)
-        countriesContainer.style.opacity = 1;
+}
+
+const renderError = function (msg) {
+    countriesContainer.insertAdjacentText('beforeend', msg);
 }
 
 const getCountryAndNeighbour = function(country) {
@@ -84,5 +87,141 @@ const getCountryAndNeighbour = function(country) {
 }
 
 //getCountryAndNeighbour('portugal')
-getCountryAndNeighbour('usa')
+//getCountryAndNeighbour('usa')
 
+// const request = fetch('https://restcountries.eu/rest/v2/name/portugal')
+// console.log(request);
+
+// AMN - above code now with fetch managing the response.
+
+// const getCountryData = function (country) {
+//     fetch(`https://restcountries.eu/rest/v2/name/${country}`) // AMN - 1st promise
+//     .then(function (response) {
+//         console.log(response)
+//         return response.json() // AMN - return data when the promise is fullfilled
+//     })
+//     .then(function(data) { // AMN - 2nd promise. The response is a promise as well that we have to manage.
+//         console.log(data);
+//         renderCountry(data[0])
+//     })
+// }
+
+// AMN - important note. The 'then' always returns a promise. In the case of response.json() the result
+// is return and catch in 'data' in the next 'then'. 
+
+const getJSON = function(url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response => {
+        if(!response.ok)
+            throw new Error(`${errorMsg} (${response.status})`) // AMn - own error
+        return response.json()
+    }
+)}
+
+// const getCountryData = function (country) {
+//     // Country 1
+//     fetch(`https://restcountries.eu/rest/v2/name/${country}`) // AMN - 1st promise...
+//     .then(response => {
+//         if(!response.ok)
+//             throw new Error(`Country not found (${response.status})`) // AMn - own error
+//         return response.json()
+//     }) // AMN - ...returns data when the promise is fullfilled
+//     .then(data => {
+//         renderCountry(data[0]);
+//         const neighbour = data[0].borders[0];
+
+//         // Country 2
+//         return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`); // AMN - 2nd promise. The response is a promise as well that we have to manage.   
+//     })
+//     .then(response => response.json())
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//         console.error(`${err} 💥 💥 💥`)
+//         renderError(`Something went wrong 💥 💥 💥 ${err.message}. Try again`)
+//     }) // AMN - catches errors - Catch itself returns a promise as well.
+//     .finally(() => { // AMN - it doesn't matter whether the promise is fullfilled or rejected this will be always executed
+//         countriesContainer.style.opacity = 1;
+//     })
+// };
+
+const getCountryData = function (country) {
+    // Country 1
+    getJSON(`https://restcountries.eu/rest/v2/name/${country}`, 'Country not found')
+    .then(data => {
+        renderCountry(data[0]);
+        const neighbour = data[0].borders[0];
+        
+        if (!neighbour) throw new Error('No neighbour found!');
+
+        // Country 2
+        return getJSON(`https://restcountries.eu/rest/v2/alpha/${neighbour}`, 'Country not found') // AMN - 2nd promise. The response is a promise as well that we have to manage.   
+    })
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+        console.error(`${err} 💥 💥 💥`)
+        renderError(`Something went wrong 💥 💥 💥 ${err.message}. Try again`)
+    }) // AMN - catches errors - Catch itself returns a promise as well.
+    .finally(() => { // AMN - it doesn't matter whether the promise is fullfilled or rejected this will be always executed
+        countriesContainer.style.opacity = 1;
+    })
+};
+
+btn.addEventListener('click', function() {
+    getCountryData('portugal');
+})
+
+//getCountryData('catalonia');
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API 
+to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country 
+name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. 
+Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. 
+Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. 
+This is an error with the request. Remember, fetch() does NOT reject the promise in this case. 
+So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, 
+and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+function whereAmI (lat, lng) {
+    console.log(lat, lng)
+    return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => {
+        if (!response.ok) throw new Error(`Problem with geocoding ${response.status}`);
+        return response.json();
+    })
+    .then(data => 
+        { if(!data.latt || data.latt === "") throw new Error(`Not data found - data: ${data}`) 
+            console.log(`You are in ${data.city}, ${data.country}`)
+        getCountryData(data.country)
+    })
+    .catch(err => console.log("The error is: ", err.message));
+}
+
+whereAmI(52.508, 13.381) // AMN - Germany
+
+whereAmI(19.037, 72.873); // AMN - India
+
+whereAmI(-33.933, 18.474); // AMN - South Africa
